0fb1c7f7c19fabd6112a852fadddc87e5049b6d6
Tom Ruff <ruffmantom@gmail.com>
2020-04-16 13:07:27 -0600

    got video background finished

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "../../components/Card";
 import ProList from "../../assets/ProList.json";
 import logo from "../../assets/images/main-logo-min.png";
+import video from "./codingBkg.mp4";
 
 console.log(ProList);
 {
@@ -15,47 +16,38 @@ console.log(ProList);
 function Projects() {
     return (
         <div>
-            <div className="row landing">
-
-                <div className="col-md-6  welcome">
-                    <h2 className="title">Ruff web Development</h2>
-                    <p className="snippet">Welcome to Tom Ruff Web Design. Please have a look around and check out my resume
-                        below.
-                    </p>
-                    <a href="#" target="_blank" className="btn btn-primary resume">Resume</a>
-                </div>
-
-                <dvi className="col-md-6 hide-small">
-
-                </dvi>
-
-
-            </div>
-            {/* projects slide effect */}
-            <div className="mainDiv">
+            <div className="projectLanding">
+                <video autoPlay muted loop className="videoBkg">
+                    <source src={video} type="video/mp4"></source>
+                </video>
                 
-                {/* this will be for the left side */}
-                <div className="leftScroll">
-                    <img className="leftLogo" src={logo} alt="Website logo"></img>
-                    <h1>Projects</h1>
-                    <p>Below you will see my personal projects and projects I have collaborated on.</p>
-                </div>
-                {/* this div will be for the right side */}
-                <div className="right">
-                    {/* in here will be all the projects mapped */}
-                    {ProList.map(res => (
-                        <Card
-                            image={res.image}
-                            name={res.name}
-                            info={res.info}
-                            link={res.link}
-                            gitLink={res.gitLink} 
-                        />
-                    ))}
+            </div>
 
+                {/* projects slide effect */}
+                <div className="mainDiv">
+
+                    {/* this will be for the left side */}
+                    <div className="leftScroll">
+                        <img className="leftLogo" src={logo} alt="Website logo"></img>
+                        <h1>Projects</h1>
+                        <p>Below you will see my personal projects and projects I have collaborated on.</p>
+                    </div>
+                    {/* this div will be for the right side */}
+                    <div className="right">
+                        {/* in here will be all the projects mapped */}
+                        {ProList.map(res => (
+                            <Card
+                                image={res.image}
+                                name={res.name}
+                                info={res.info}
+                                link={res.link}
+                                gitLink={res.gitLink}
+                            />
+                        ))}
+
+                    </div>
                 </div>
             </div>
-        </div>
 
     )
 }
@@ -63,5 +55,5 @@ function Projects() {
 var leftDiv = document.getElementsByClassName("left");
 document.addEventListener("scroll", function(){
 
-})
+            })
 export default Projects;
