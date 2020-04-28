912d8b23851da0e317f79347f00b7c49cdf1bfda
Tom Ruff <ruffmantom@gmail.com>
2020-04-28 11:19:12 -0600

    changed classnames for project cards and homepage cards

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -5,7 +5,7 @@ const url = "https://drive.google.com/uc?id=";
 function Card(props) {
     return (
         <div className="rightCard">
-            <img className="card-img" src={url + props.image} alt="Project Screen shot"></img>
+            <img className="proCard-img" src={url + props.image} alt="Project Screen shot"></img>
             <h3 className="card-title">{props.name}</h3>
             <p className="card-snippet">{props.info}</p>
             <a href={props.link} className="dep-btn btn btn-primary">Check it Out!</a> <br></br>
