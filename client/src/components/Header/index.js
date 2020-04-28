c7f4dda5ea1b6a0814b6133692cc9cc4e9c68204
Tom Ruff <ruffmantom@gmail.com>
2020-04-28 11:48:07 -0600

    added new resume fixed a couple styles

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -9,7 +9,7 @@ import {Navbar, Nav} from "react-bootstrap";
 
 function Header() {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="lg">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
